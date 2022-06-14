624cce7a291eee60c9ad5cd24bf8e2cf96f15bba
Aran <96021763+Aran404@users.noreply.github.com>
2022-06-14 19:18:37 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,34 +1,5 @@
 @echo off
-SETLOCAL
-
-cls||clear
-set /P User_Os=Is your OS windows *w* or other *o*: 
-
-if /i %User_Os% == w call :Windows 
-if /i %User_Os% == o call :others
-
-echo %User_Os% Is not a valid Operating System
-pause
-exit
-
-:Windows
-cls||clear
-title Installing Packages
-py -3 -m pip install -r requirements.txt
-cls||clear
-title Installed Packages
-@echo Installed All Packages
-pause
-
-:others
-cls||clear
-title Installing Packages
-python3 -m pip install -r requirements.txt
-cls||clear
-title Installed Packages
-@echo Installed All Packages
-pause
-
+py -m pip install -r requirements.txt
 cls
 echo py account_creator.py >> run.bat
 echo pause >> run.bat
